023a8b44e17c6ed7fe6d9c75d529d3a60e84a476
Jälv <thomas.montaigu@laposte.net>
2019-06-28 22:09:23 +0200

    move print-content example

---
+++ examples/print-content/src/main.rs
@@ -0,0 +1,34 @@
+extern crate shapefile;
+use std::env;
+use std::process::exit;
+use std::mem::size_of;
+
+fn main() {
+    let args: Vec<String> =  env::args().into_iter().collect();
+    let filename = match args.get(1) {
+        Some(arg) => arg,
+        None => {
+            println!("Expected a path to a file as first argument.");
+            exit(-1);
+        }
+    };
+
+    let reader = shapefile::Reader::from_path(filename).unwrap();
+    for (i, shape) in reader.into_iter().enumerate() {
+        let shape = shape.unwrap();
+    }
+
+    let reader = shapefile::FileReaderBuilder::new(filename)
+        .with_dbf()
+        .build()
+        .unwrap();
+
+    for result in reader.iter_shapes_and_records().unwrap() {
+        let (shape, record) = result.unwrap();
+        println!("Shape: {}, records: ", shape);
+        for (name, value) in record {
+            print!("{}: {:?}, ", name, value);
+        }
+        println!("");
+    }
+}
