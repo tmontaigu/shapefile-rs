314ea7abc7e8f16f0e6197bc59cb769b9117848c
tmontaigu <thomas.montaigu@laposte.net>
2021-06-14 22:25:32 +0200

    Use re exported dbase

--- examples/geotype-example/src/main.rs
+++ examples/geotype-example/src/main.rs
@@ -1,4 +1,8 @@
 use geo::prelude::Contains;
+// shapefile re-exports dbase so you can use it
+use shapefile::dbase;
+
+
 fn main() {
     let polygons = shapefile::read_as::<_, shapefile::Polygon, shapefile::dbase::Record>(
         "./tests/data/polygons.shp",
@@ -14,12 +18,12 @@ fn main() {
             let geo_point: geo::Point<f64> = point.clone().into();
             if geo_polygon.contains(&geo_point) {
                 let point_id = match point_record.get("id") {
-                    Some(shapefile::dbase::FieldValue::Numeric(Some(x))) => x,
+                    Some(dbase::FieldValue::Numeric(Some(x))) => x,
                     Some(_) => panic!("Expected 'id' to be a numeric in point-dataset"),
                     None => panic!("Field 'id' is not within point-dataset"),
                 };
                 let polygon_id = match polygon_record.get("id") {
-                    Some(shapefile::dbase::FieldValue::Numeric(Some(x))) => x,
+                    Some(dbase::FieldValue::Numeric(Some(x))) => x,
                     Some(_) => panic!("Expected 'id' to be a numeric in polygon-dataset"),
                     None => panic!("Field 'id' is not within polygon-dataset"),
                 };
