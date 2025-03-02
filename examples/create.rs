50e1f8397d23d8a26e9b68aa0b387599e595914e
tmontaigu <thomas.montaigu@laposte.net>
2025-03-02 15:43:52 +0100

    Add examples on how to create files

---
+++ examples/create.rs
@@ -0,0 +1,107 @@
+use dbase::{dbase_record, TableWriterBuilder};
+use shapefile::{dbase, Point, Polyline, Writer};
+
+
+fn example_1() {
+    const FILE_NAME: &'static str = "hello_shape_1.shp";
+
+    let square = Polyline::new(vec![
+        Point::new(0.0, 0.0),
+        Point::new(0.0, 1.0),
+        Point::new(1.0, 1.0),
+        Point::new(1.0, 0.0),
+    ]);
+
+    let bigger_square = Polyline::new(vec![
+        Point::new(0.0, 0.0),
+        Point::new(0.0, 10.0),
+        Point::new(10.0, 10.0),
+        Point::new(10.0, 0.0),
+    ]);
+
+    // Create the builder for the accompanying dbase (.dbf) file
+    // For this simple example we will only have a single field
+    // "Name", with 55 chars max
+    let table_builder =
+        TableWriterBuilder::new().add_character_field("Name".try_into().unwrap(), 55);
+
+    {
+        let mut writer = Writer::from_path(FILE_NAME, table_builder)
+            .expect("Failed to create the shapefile writer");
+
+        let mut first_record = dbase::Record::default();
+        first_record.insert("Name".to_string(), "Square".to_string().into());
+        writer
+            .write_shape_and_record(&square, &first_record)
+            .expect("Failed to write first record");
+
+        let mut second_record = dbase::Record::default();
+        second_record.insert("Name".to_string(), "Big Square".to_string().into());
+        writer
+            .write_shape_and_record(&bigger_square, &second_record)
+            .expect("Failed to write second record");
+    }
+
+    println!("File created, you can use `cargo run --example print-content {FILE_NAME}`");
+}
+
+fn example_2() {
+    dbase_record!(
+        #[derive(Debug)]
+        struct UserRecord {
+            first_name: String,
+            last_name: String,
+        }
+    );
+
+    const FILE_NAME: &'static str = "hello_shape_2.shp";
+
+    let square = Polyline::new(vec![
+        Point::new(0.0, 0.0),
+        Point::new(0.0, 1.0),
+        Point::new(1.0, 1.0),
+        Point::new(1.0, 0.0),
+    ]);
+
+    let bigger_square = Polyline::new(vec![
+        Point::new(0.0, 0.0),
+        Point::new(0.0, 10.0),
+        Point::new(10.0, 10.0),
+        Point::new(10.0, 0.0),
+    ]);
+
+    // Create the builder for the accompanying dbase (.dbf) file
+    let table_builder =
+        TableWriterBuilder::new()
+            .add_character_field("FirstName".try_into().unwrap(), 55)
+            .add_character_field("LastName".try_into().unwrap(), 55);
+
+    {
+        let mut writer = Writer::from_path(FILE_NAME, table_builder)
+            .expect("Failed to create the shapefile writer");
+
+        let first_record = UserRecord {
+            first_name: "Yoshi".to_string(),
+            last_name: "Green".to_string(),
+        };
+
+        writer
+            .write_shape_and_record(&square, &first_record)
+            .expect("Failed to write first record");
+
+        let second_record = UserRecord {
+            first_name: "Yoshi".to_string(),
+            last_name: "Red".to_string(),
+        };
+        writer
+            .write_shape_and_record(&bigger_square, &second_record)
+            .expect("Failed to write second record");
+    }
+
+    println!("File created, you can use `cargo run --example print-content {FILE_NAME}`");
+}
+
+fn main() {
+    example_1();
+    example_2();
+}
---
+++ examples/print-content.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::process::exit;
+
+fn main() {
+    let args: Vec<String> = env::args().into_iter().collect();
+    let filename = match args.get(1) {
+        Some(arg) => arg,
+        None => {
+            println!("Expected a path to a file as first argument.");
+            exit(-1);
+        }
+    };
+
+    let mut reader = shapefile::Reader::from_path(filename).unwrap();
+
+    for result in reader.iter_shapes_and_records() {
+        let (shape, record) = result.unwrap();
+        println!("Shape: {}, records: ", shape);
+        for (name, value) in record {
+            println!("\t{}: {:?}, ", name, value);
+        }
+        println!();
+    }
+}
